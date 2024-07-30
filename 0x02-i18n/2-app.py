90417a2880b5d82be48fd7b84bf7de4f952996c0
Gloireski <35967799+Gloireski@users.noreply.github.com>
2024-07-30 17:19:29 +0100

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,7 +19,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """ fn to determines the best match with our supported languages """
     return request.accept_languages.best_match[Config.LANGUAGES]
 
